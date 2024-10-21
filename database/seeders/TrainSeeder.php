b9bdb4f5272e5980bea38ff2483dad08ebbe083b
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-10-21 15:26:05 +0200

    Completato esercizio

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Model\Train;
+
 class TrainSeeder extends Seeder
 {
     /**
@@ -12,6 +14,20 @@ class TrainSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Train::truncate();
+
+        for ($i=0; $i < 10; $i++) { 
+            $train = new Train();
+            $train->code = strtopupper(fake()->botihify('?#?#?#?#'));
+            $train->group = fake()->group();
+            $train->strt_station = fake()->city();
+            $train->arr_station = fake()->city();
+            $train->strt_time = fake()->time();
+            $train->arr_time = fake()->time();
+            $train->carriages_number = rand(2, 10);
+            $train->on_time = fake()->boolean(60);
+            $train->cancelled = fake()->boolean(10);
+            $train->save();
+        }
     }
 }
