593919621100626e284ebc83765ca6a0c9c33bc0
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-10-21 14:42:20 +0200

    create tabelle

---
+++ database/migrations/2024_10_21_122237_create_trains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('group', 64);
+            $table->string('strt_station', 64);
+            $table->string('arr_station', 64);
+            $table->time('strt_time');
+            $table->time('arr_time');
+            $table->tinyInteger('carriages_number')->nullable()->unsigned();
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
